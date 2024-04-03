700322faf2fec44e6019dd3f3a4617ffa8b75ba0
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-03 12:24:31 +0200

    Segunda Demo

--- 02/modulo2/ConsoleApp1/ConsoleApp1/Program.cs
+++ 02/modulo2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,12 +6,19 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed; ;
+            //Calculated values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed; 
             double unanweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.PriceScore + Q1Results.FoodScore) / 4;
+
+
+
+
 
             Console.WriteLine($"Response Percentage: {responseRate}");
             Console.WriteLine($"Unanswered Surveys: {unanweredCount}");
-
+            Console.WriteLine($"Overall Score: {overallScore}");
 
 
         }
