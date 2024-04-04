ba487ba96488a105970b963d4178d3956d46ce60
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-04 08:41:16 +0200

    Segunda demo modulo 5

--- 05/Modulo5/Modulo5/obj/Debug/net8.0/Modulo5.AssemblyInfo.cs
+++ 05/Modulo5/Modulo5/obj/Debug/net8.0/Modulo5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modulo5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4de117113931dd224aed761a7886898c9832e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9833af45d8737d7aea24f5ca97b820786c41205")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modulo5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modulo5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
